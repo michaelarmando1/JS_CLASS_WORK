b04df59bb7d48d0ce66db973bb92e80e778cc9d5
michaelamando1 <michaelamando25@gmail.com>
2024-03-27 13:18:50 +0100

    first commit

---
+++ variables.js
@@ -0,0 +1,96 @@
+// let word = "Akpan is a greedy man";
+
+// word = "Akpan likes money too much. But he gave you collected Maya";
+
+// console.log(word);
+
+// const newWord = "Michael Likes Joyce";
+
+// newWord = "Michael Likes Maya";
+
+
+// console.log(newWord);
+
+let singer = "Beyonce";
+
+let singerLength = singer.length;
+
+// let singerIndexOf = singer.indexOf("e", 2);
+
+// console.log(singerIndexOf);
+
+// console.log(singerLength);
+
+// let musician = "The Chain Smokers";
+
+// let sliceMusician = musician.slice()
+
+let nameOfMusician = "drake";
+
+let toUpper = nameOfMusician.charAt(0).toUpperCase();
+
+// let upperC = toUpper.toUpperCase();
+
+console.log(toUpper);
+
+
+// Number Methods
+
+let number =  1234.25;
+
+let toString = number.toString();
+
+let toFixed = number.toFixed(19);
+
+console.log(toFixed);
+console.log(number);
+console.log(toString);
+
+let numPre = 234.56;
+let toPrecision = numPre.toPrecision(3);
+console.log(toPrecision);
+
+let toExponential = numPre.toExponential(1);
+
+console.log(toExponential);
+
+let valnum = 12.00;
+console.log(valnum.valueOf());
+
+//Math Methods
+
+let newNum = 144;
+
+let sqrt = Math.sqrt(newNum);
+console.log(sqrt);
+
+let pow = Math.pow(newNum, 2);
+console.log(pow);
+
+let sinNum = 0;
+let sin = Math.cos(sinNum);
+console.log(sin);
+
+let logVal = 2;
+let log = Math.log10(logVal);
+console.log(log);
+
+let Pi = Math.PI;
+
+let cir = 2 * Pi * Math.pow(7,2);
+console.log(cir);
+
+let min = Math.max(1, 2, 3, 4, 5, 6, -5);
+console.log(min);
+
+let trunc = Math.trunc(123.96);
+console.log(trunc);
+
+let random = Math.floor(Math.random() * 1000000);
+console.log(random);
+
+
+
+
+//Assignment, Create a function that splits words into half or returns the first half of a string
+//Assignment2, "Hello World", "Hello Life". return the length of the words excluding the spaces
