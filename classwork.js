2f256b67165f0bccd328eb9da736a77680f7e84f
michaelamando1 <michaelamando25@gmail.com>
2024-04-15 10:56:20 +0100

    Javascript Ongoing Class

---
+++ classwork.js
@@ -0,0 +1,44 @@
+// No 1
+
+let a = "kola";
+
+let reverseA = a.split("").reverse().join("");
+
+console.log(reverseA);
+
+// No 2
+
+let v = "gideon";
+
+let toUpper = v.charAt(0).toUpperCase() + v.slice(1);
+
+console.log(toUpper);
+
+// No 3
+
+let paWord = "Mom"; 
+
+let checker = paWord.che
+
+// No 4
+
+let obj = {name : "Paul"};
+
+obj.name = "Peter";
+
+console.log(obj);
+
+// No 5
+
+let str = "I am Fola, I love Fishing";
+
+let newStr = str.replace("Fola", "Cynthia");
+
+let newStr2 = newStr.replace("love", "hate");
+
+let newStr3 = newStr2.replace("Fishing", "Swimming")
+
+
+console.log(newStr3);
+
+
---
+++ function.js
@@ -0,0 +1,23 @@
+// FUNCTIONS Declarations
+
+function world() {
+    return "Hello World";
+}
+
+console.log(world());
+
+// function Expression
+
+const greet = function () {
+    return "Hello World 2";
+}
+
+console.log(greet());
+
+// Arrow Function
+
+const newGreet = () => {
+    return "Hello World 3";
+}
+
+console.log(newGreet());
